84f97e831c3cbff4b5af4812359ce0dceb6ffeba
Stephen Broadhurst <stephen@union25.com>
2020-09-24 13:13:03 -0400

    posts.test works

--- src/tests/Posts.test.js
+++ src/tests/Posts.test.js
@@ -2,51 +2,39 @@ import React from 'react'
 import { Provider } from 'react-redux'
 import Posts from '../components/Posts'
 import { render } from '@testing-library/react'
-import Enzyme from 'enzyme'
-import Adapter from 'enzyme-adapter-react-16'
 import configureStore from 'redux-mock-store'
-import { fetchForms } from '../actions/postActions'
 import thunk from 'redux-thunk'
 
 const middlewares = [thunk]
 
 const mockStore = configureStore(middlewares)
 
-Enzyme.configure({ adapter: new Adapter() })
-
 describe('Posts component', () => {
-  const data = [{ firstName: 'test', lastName: 'testlast', email: 'email@email.com', date: '2020-11-11' }]
-  let store = mockStore({
-    posts: {
-      items: [],
-      item: {},
-    },
-  })
-
-  // function fetchForms() {
-  //   console.log('fetch forms inside here')
-  // }
-
-  let component = render(
-    <Provider store={store}>
-      <Posts fetchForms={fetchForms} />
-    </Provider>
-  )
-
-  beforeEach(() => {
-    store = mockStore({
+  it('test userinfo fields', async () => {
+    const data = [{ firstName: 'testFirst', lastName: 'testlast', email: 'email@email.com', date: '2020-11-11' }]
+    let store = mockStore({
       posts: {
-        items: [],
+        items: data,
         item: {},
       },
     })
-  })
 
-  it('should check `componentDidMount()`', () => {
-    const spy = jest.spyOn(Posts.WrappedComponent.prototype, 'componentDidMount')
+    const { getByText } = render(
+      <Provider store={store}>
+        <Posts />
+      </Provider>
+    )
 
-    const methodNameFake = jest
-      .spyOn(component.prototype, 'fetchForms')
-      .mockImplementation(() => console.log('this is the mock'))
+    const firstName = getByText(/testFirst/i)
+    expect(firstName).toBeInTheDocument()
+
+    const lastName = getByText(/testLast/i)
+    expect(lastName).toBeInTheDocument()
+
+    const email = getByText(/email@email.com/i)
+    expect(email).toBeInTheDocument()
+
+    const date = getByText(/2020/i)
+    expect(date).toBeInTheDocument()
   })
 })
